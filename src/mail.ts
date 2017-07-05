199121c6fad68669a462ecdfd43135b756c6c689
pboymt <pboymt@foxmail.com>
2017-07-05 21:03:38 +0800

    add more log

--- src/mail.ts
+++ src/mail.ts
@@ -18,14 +18,14 @@ interface smtpConfig {
 export async function sendMail(filepath: string, config: smtpConfig = null) {
     const Yesterday = new Date(Date.now() - 1000 * 60 * 60 * 24);
     const smtpConfig: smtpConfig = require(join(__dirname, '../smtpconfig.json'));
-    console.log(smtpConfig);
+    log(smtpConfig);
     config = config || smtpConfig;
     const smtpTransport = createTransport(config);
     try {
         const sentMessageInfo = await smtpTransport.sendMail({
             from: '"user" <' + config.auth.user + '>',
             to: config.to,
-            subject: `动漫花园${Yesterday.getFullYear()}年${String(Yesterday.getMonth() + 1).padStart(2, '0')}月${String(Yesterday.getDate() + 1).padStart(2, '0')}日种子合集`,
+            subject: `动漫花园${Yesterday.getFullYear()}年${String(Yesterday.getMonth() + 1).padStart(2, '0')}月${String(Yesterday.getDate()).padStart(2, '0')}日种子合集`,
             // subject: '动漫花园' + new Date().getFullYear() + '年' + (new Date().getMonth() * 1 + 1) + '月' + (new Date().getDate() * 1 - 1) + '日种子合集',
             text: '这是一封测试邮件',
             attachments: [{
--- src/pack.ts
+++ src/pack.ts
@@ -1,7 +1,10 @@
 import { zip } from "./zip";
 import { sendMail } from "./mail";
+import { log } from "./log";
 
 (async () => {
     const [filename, number] = await zip();
+    log(`Add ${number} Torrent Files to Zip`);
+    log(`Zip saved at ${filename}`);
     await sendMail(filename);
 })();
